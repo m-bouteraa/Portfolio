a9878458f207023ad5dae9ef3eac0fb9986986a1
m-bouteraa <m-bouteraa@outlook.com>
2025-03-21 20:55:12 +0100

    Update contact links

--- edit-me/config/links.ts
+++ edit-me/config/links.ts
@@ -7,17 +7,17 @@ import { MaltSvg } from 'src/components/Icons/Icons';
 
 export const links: CMSLink[] = [
   {
-    href: 'mailto:ronan.jaffre@rjconsulting.fr',
+    href: 'mailto:contact@mad-consulting.fr',
     customIcon: SiGmail,
     title: 'Email',
   },
   {
-    href: 'https://www.linkedin.com/in/rjaffre',
+    href: 'https://www.linkedin.com/in/mohamed-bouteraa-93982b116',
     icon: SiLinkedin,
     title: 'LinkedIn',
   },
   {
-    href: 'https://www.malt.fr/profile/ronanjaffre',
+    href: 'https://www.malt.fr/profile/mohamedbouteraa',
     customIcon: MaltSvg,
     title: 'Malt',
   },
